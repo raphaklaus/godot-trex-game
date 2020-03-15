9decc5b5ac56f4ef85dd67d2250b0f37e41cdaca
Raphael <raphaklaus@gmail.com>
2020-03-15 13:27:09 +0100

    chore: add HTML5 as mobile

--- controller.gd
+++ controller.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if OS.get_name() == "Android":
+	if OS.get_name() == "Android" or OS.get_name() == "HTML5":
 		visible = true
 
 
